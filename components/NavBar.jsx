f8e276bff0574e8098c8418ff9697038316d8e0c
0xSunil <sunday7637@gmail.com>
2022-12-11 20:32:34 +0530

    added responsive navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,12 +1,57 @@
 import { logo, close, menu } from "../public/assets";
-import { airbnb } from "../public/assets";
+
 import { navLinks } from "../Data";
-import logo2 from '../public/assets/index'
+
+import Image from "next/image";
+import { use, useState } from "react";
+
 const NavBar = () => {
+  const [tottle, setTottle] = useState(false);
   return (
-    <nav className="w-full flex py-0 justify-between items-center navbar">
-      <img src={airbnb} alt="hoobank" />
-      <img src={logo2} alt="logo" />
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <Image src={logo} alt="logo" className=" w-[124px]" />
+      <ul className="list-none hidden sm:flex justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <Image
+          src={tottle ? close : menu}
+          alt="menu"
+          className="w-[25px] h-[25px] object-contain"
+          onClick={() => {
+            setTottle((tog) => {
+              return !tog;
+            });
+          }}
+        />
+        <div
+          className={`${
+            tottle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex  justify-end flex-col items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-10"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
